54a977ff91cfe92d5b464c64d7d17106ebf5c45f
rdlsy <lsy_65@naver.com>
2020-10-22 16:54:12 +0900

    경로수정

--- src/api.js
+++ src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const config1 = {
-  API_ENDPOINT: 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
+  API_ENDPOINT: 'https://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json',
   KEY: '664383b11fdaa853a4884a63b0ae7cc4'
 }
 const config2 = {
-  API_ENDPOINT: 'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2',
+  API_ENDPOINT: 'https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2',
   KEY: '229UV0UP2N545R3W7SN2'
 }
 const config3 = {

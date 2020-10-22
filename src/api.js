9a01a89e336f3306e66375744ee1d69e2931b7c4
rdlsy <lsy_65@naver.com>
2020-10-22 17:02:23 +0900

    검색결과 수정

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ const config1 = {
   KEY: '664383b11fdaa853a4884a63b0ae7cc4'
 }
 const config2 = {
-  API_ENDPOINT: 'https://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2',
+  API_ENDPOINT: 'http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2',
   KEY: '229UV0UP2N545R3W7SN2'
 }
 const config3 = {

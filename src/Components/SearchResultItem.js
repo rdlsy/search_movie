d6eb61bb50b313215636c0bfa51864078b22ac30
rdlsy <lsy_65@naver.com>
2020-10-26 19:01:38 +0900

    상세 정보 추가

--- src/Components/SearchResultItem.js
+++ src/Components/SearchResultItem.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 function SearchResultItem({ movie }) {
-  const { title, posters, type, nation, runtime, titleEng, rating, director, actor } = movie;
+  const { title, posters, type, nation, runtime, repRlsDate, titleEng, prodYear, rating, company, director, actor, stlls } = movie;
   const poster = posters.split('|')[0];
+  const still = stlls.split('|');
   const directorNm = director[0].directorNm;
   const actors = actor.splice(0, 5);
+  //actors = actor.splice(0, 5);
   const noImage = '/search_movie/img/noimage.gif';
   const ratingGrade = rating[0].ratingGrade;
   let real_title = title.replaceAll(' !HS ', '');
   real_title = real_title.replaceAll(' !HE ', '');
-
-  console.log(movie)
+  let real_repRlsDate = repRlsDate.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
 
   return (
     <div className="Movie">
@@ -22,6 +23,18 @@ function SearchResultItem({ movie }) {
         <div className="Movie_Info">
           <ul className="Movie_Info_List">
             <li>{type} · <span>{ratingGrade}</span> · {nation + ' · ' + runtime + '분'}</li>
+            <li>
+              <dl>
+                <dt>개봉일</dt>
+                <dd>{real_repRlsDate}</dd>
+              </dl>
+            </li>
+            <li>
+              <dl>
+                <dt>제작사</dt>
+                <dd>{company}</dd>
+              </dl>
+            </li>
             <li>
               <dl>
                 <dt>감독</dt>
